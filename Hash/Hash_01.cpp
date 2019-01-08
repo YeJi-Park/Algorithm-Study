addab6f38e5bb343a63ad2670b8ac9f5ab5c9fc8
YeJi-Park <tuilte38@gmail.com>
2019-01-08 18:28:48 +0900

    first commit

---
+++ Hash/Hash_01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace::std;
+/*
+	@Author		YJ Park
+	@Date		19. 01. 08
+	@Descript	Programmers 알고리즘 해시 #01 sort 
+*/
+
+string solution(vector<string> participant, vector<string> completion) {
+	sort(participant.begin(), participant.end());
+	sort(completion.begin(), completion.end());
+
+	int size = participant.size(), i = 0;
+	for (i; i < size; i++) {
+		if (participant[i] != completion[i]) break;
+	}
+
+	return participant[i];
+}
