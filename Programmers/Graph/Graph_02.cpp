a71b62ec97df697cf462d94106ecad89bd884bb4
YeJi-Park <tuilte38@gmail.com>
2019-02-07 13:28:17 +0900

    그래프_02 수정

--- Programmers/Graph/Graph_02.cpp
+++ Programmers/Graph/Graph_02.cpp
@@ -1,17 +1,19 @@
 /*
 @Author		YJ Park
-@Date		19. 02. 06
+@Date		19. 02. 07
 @Descript	Programmers Graph #02
 			1.	중복 제거를 위해 사용한 set 자료구조가 자동정렬이라
 				선수번호가 앞인 경우 놓쳐서 test case 2에서 실패
+			2.	visit(방문한 원소 수), elem_num(세트에 들어있는 원소 수) 변수를 세서
+				visit<elem_num인 동안 계속 순회하도록 수정
 */
 
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <set>
 
 using namespace std;
-
 
 int solution(int n, vector<vector<int>> results) {
 	int answer = 0;
@@ -25,25 +27,72 @@ int solution(int n, vector<vector<int>> results) {
 		lose[results[i][1]].insert(results[i][0]);
 	}
 
-	for (int i = 1; i < n+1; i++) {
-		for (auto j : lose[i])
-			for (auto k : lose[j])
-				lose[i].insert(k);
+	for (int i = 1; i < n + 1; i++) {
+		vector<bool> visited(n + 1, false);
+		int elem_num = lose[i].size();
+		int visit = 0;
+
+		for (auto j : lose[i]) {
+			visited[j] = true;
+			visit++;
+
+			for (auto k : lose[j]) {
+				if (lose[i].insert(k).second)
+					elem_num++;
+			}
+		}
+
+		while (elem_num > visit) {
+			for (auto j : lose[i]) {
+				if (visited[j]) continue;
+				else {
+					visited[j] = true;
+					visit++;
+
+					for (auto k : lose[j]) {
+						if (lose[i].insert(k).second)
+							elem_num++;
+					}
+
+				}
+			}
+		}
 	}
 
 	for (int i = 1; i < n + 1; i++) {
-		for (auto j : win[i])
-			for (auto k : win[j])
-				win[i].insert(k);
+		vector<bool> visited(n + 1, false);
+		int elem_num = win[i].size();
+		int visit = 0;
+
+		for (auto j : win[i]) {
+			visited[j] = true;
+			visit++;
+
+			for (auto k : win[j]) {
+				if (win[i].insert(k).second)
+					elem_num++;
+			}
+		}
+
+		while (elem_num > visit) {
+			for (auto j : win[i]) {
+				if (visited[j]) continue;
+				else {
+					visited[j] = true;
+					visit++;
+
+					for (auto k : win[j]) {
+						if (win[i].insert(k).second)
+							elem_num++;
+					}
+
+				}
+			}
+		}
 	}
 
 	for (int i = 1; i < n + 1; i++)
-		if (win[i].size() + lose[i].size() == (n-1)) answer++;
+		if (win[i].size() + lose[i].size() == (n - 1)) answer++;
 
 	return answer;
 }
-
-int main() {
-	cout << solution(5,{ {4, 3}, { 4, 2 }, { 3, 2 }, { 1, 2 }, { 2, 5 }});
-}
-
